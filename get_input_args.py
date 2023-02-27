cbe6cf1bbd77695127332ec71fb03d066b7be03b
Edwin <starlest@gmail.com>
2023-02-27 21:53:39 +0700

    update argument parser to include help for arguments

--- get_input_args.py
+++ get_input_args.py
@@ -43,9 +43,9 @@ def get_input_args():
     parser = argparse.ArgumentParser()
     
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    parser.add_argument('--dir', default="pet_images")
-    parser.add_argument('--arch', default="vgg")
-    parser.add_argument('--dogfile', default="dognames.txt")
+    parser.add_argument('--dir', type=str, default="pet_images/", help="path to the folder of pet images")
+    parser.add_argument('--arch', type=str, default="vgg", help="the CNN Model Architecture")
+    parser.add_argument('--dogfile', type=str, default="dognames.txt", help="path to text file with dog names")
     
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
--- get_pet_labels.py
+++ get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Edwin Chia
+# DATE CREATED: 27-02-2023                         
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
@@ -40,6 +40,10 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
+    results_dic = {}
+    
+    
+    
     # Replace None with the results_dic dictionary that you created with this
     # function
     return None
